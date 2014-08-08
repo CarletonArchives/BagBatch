d876fca6dcd282a93ec54c2384a2215434152fc6
sahreek <sahreek@gmail.com>
2014-08-08 16:10:59 -0500

    Initial commit

---
+++ bagbatch.py
@@ -0,0 +1,131 @@
+"""
+	bagbatch.py
+	Written to run BagIt on multiple directories at once.
+	August 2014
+
+	Usage:	python bagbatch.py <dir>
+			<dir> is the parent of the subdirectories to bag
+			BAGIT_INST_PATH.txt will contain the installation path to bag.bat/bag.sh
+
+	Input:		Parent directory with subdirectories to be bagged.
+	Output:		Parent directory with bagged subdirectories.
+	Dependency: BAGIT_INST_PATH.txt
+		The first time bagbatch.py is run, it will create BAGIT_INST_PATH.txt 
+		and prompt for the path to bag.bat or bag.sh. This path may be 
+		"/Program Files/bagit/bin" or "C:\Program Files\bagit\bin". The path 
+		will be saved in BAGIT_INST_PATH.txt. Each time bagbatch.py is run, it 
+		will validate this location. 
+"""
+
+import os, sys, platform
+import shlex, subprocess
+
+BAGIT_INST_PATH = "BAGIT_INST_PATH.txt"
+BAGBATCH_DIR = os.getcwd()
+
+def call_bag_baginplace(directory, timeout = None):
+	""" Given a single directory, calls bag baginplace <dir> using 
+	subprocess.check_call() for Windows and subprocess.Popen() for Mac. Errors  
+	will be printed out. """
+	print '----------------------\nBagging', directory + "."	
+	with open(BAGIT_INST_PATH) as bb_path:
+		path = bb_path.read()
+
+	os.chdir(path)
+	try:
+		if get_ext() == '.bat':
+			subprocess.check_call([os.path.join(path, 'bag'), 'baginplace', directory], shell=True)
+			return True
+		elif get_ext() == '.sh':
+			p = subprocess.Popen(['./bag','baginplace',directory])
+			p.wait()
+			stdout, stderr = p.communicate()
+			# if nothing goes wrong, stdout and stderr = None
+			if stdout:
+				print stdout
+			if stderr:
+				print stderr
+			return True
+		else:
+			print 'Error detecting OS'
+			return False
+		
+	except:
+		print "Error: Directory not bagged. Make sure BagIt is installed correctly \
+		and the directory path listed in", BAGIT_INST_PATH, "contains bag" + get_ext()
+		return False
+
+
+def get_immediate_subdirectories(directory):
+	""" Given a directory name, finds all immediate subdirectories and returns
+	the complete directory paths in a list. source: 
+	http://stackoverflow.com/a/800201 """
+	dirNames = [name for name in os.listdir(directory)
+	if os.path.isdir(os.path.join(directory, name))]
+	dirList = []
+	for d in dirNames:
+		dirList.append(os.path.join(directory,d))
+	return dirList
+
+def validate_bagit_path():
+	""" Validates the path to bag.bat/bag.sh and resets it if needed. """
+	print "\nValidating BagIt installation path..."
+	# Creates the text file with the bagit path, if need be
+	if not os.path.isfile(BAGIT_INST_PATH):
+		bb_file = open(BAGIT_INST_PATH, "w")
+		bb_file.close()
+
+	# closes file after with statement
+	with open(BAGIT_INST_PATH, "r+") as bb_path:
+		path = bb_path.read()
+	
+	# if the path exists and the files are present, return and start bagging
+	if path and os.path.exists(path):
+		if os.path.isfile(os.path.join(path,'bag')):
+			print "Validated.\n"
+			return
+		else:
+			print 'cannot find extension'
+
+	# if not, prompt for new path, write to BAGIT_INST_PATH.txt, and validate
+	path = str(raw_input('Enter the BagIt installation path containing bag%s (ex: "C:\\Program Files\\bagit-x.x.x\\bin" or "/Program Files/bagit-x.x.x/bin"): '%get_ext())) 
+	# if there are extra quoting characters, remove them
+	if '"' in path or "'" in path:
+		path=''.join(shlex.split(path))
+	bb_file = open(BAGIT_INST_PATH, "w")
+	bb_file.write(path.strip())
+	bb_file.close()
+	validate_bagit_path()
+
+def get_ext():
+	""" Based on the operation system, returns the correct file extension for. """
+	if 'Windows' in platform.system() or 'win32' == sys.platform:
+		return '.bat'
+	else:
+		return '.sh'
+	
+def usage_message():
+	print "Usage:\tpython bagbatch.py <dir>\n",
+	print "\t<dir> is the parent directory of the subdirectories to bag"
+	print "\tBAGIT_INST_PATH.txt will contain the installation path to bag%s" %get_ext()
+
+def main():
+	if len(sys.argv) <= 1 or not os.path.exists(sys.argv[1]):
+		return usage_message()
+
+	validate_bagit_path()
+
+	dirToBag = sys.argv[1]
+	print "----------------------\nCreating bags for all directories in", 
+	print dirToBag + ".\n----------------------"
+	success = True
+	for d in get_immediate_subdirectories(dirToBag):
+		# calls 'bag baginplace <dir>' for each directory
+		if not call_bag_baginplace(d):
+			success = False
+		os.chdir(BAGBATCH_DIR)
+	if success:
+		print "----------------------\n----------------------\nBags Complete.\n----------------------"
+	else:
+		print "----------------------\n----------------------\nBags not complete: Error with bags.\n----------------------"
+main()
---
+++ readme.txt
@@ -0,0 +1,99 @@
+BagIt batch processing in Python
+
+bagbatch.py
+    Written to run BagIt on multiple directories at once.
+    August 2014
+
+
+BagIt and bagbatch.py
+---------------------
+BagIt is a Java-based program that creates archival bags of files following 
+the BagIt standard developed by the Library of Congress. It is used to store a 
+directory's master copies, web copies, and metadeta or to verify that files 
+have been transfered from one computer to another properly. By default, BagIt 
+only processes one directory at a time. bagbatch.py was written to process an 
+entire parent directory with folders ready for bagging on both Mac and Windows.
+
+
+
+Requirements and setup
+----------------------
+ - BagIt
+ - Java
+ - Python 2.7
+
+[Carlpedia - BagIt](https://wiki.carleton.edu/display/carl/BagIt) has steps on setting 
+up Java and BagIt.
+
+
+BAGIT
+
+BagIt can be downloaded from the [Library of Congress on GitHub] (https://github.com/LibraryOfCongress/bagit-java). Adapted from the readme file for BagIt 4.9.0 (see the link above for the most recent information):
+   |  Unzip bagit-java-master. From this new folder, unzip bagit-x.x.x-src.zip.
+   |  In the resulting 'bagit-x.x.x/bin' subdirectory, you will find bag.sh 
+   |  (Mac) and bag.bat (Windows) scripts that are used to invoke the 
+   |  commandline interface.
+
+JAVA
+
+Java must be installed and the Java runtime environment (JAVA_HOME) must be 
+set up correctly for BagIt. To check if Java is installed, type "java -version"
+(without the quotes) in the terminal (Mac) or command prompt (Windows). If 
+Java is not installed, download it from java.com. 
+
+PYTHON
+
+Python must be installed to run bagbatch.py. Enter "python" into the terminal 
+or command prompt to see if Python is setup for terminal/command line use. If 
+not, make sure Python is installed (if it isn't, download version 2.7 from 
+python.org). [Python documentation for setting up the interpreter](https://docs.python.org/2.7/tutorial/index.html)
+
+
+
+Using bagbatch.py
+-----------------
+OVERVIEW
+
+1. Using the terminal or command prompt, navigate to the directory containing 
+bagbatch.py
+2. Enter "python bagbatch.py <dir>" where <dir> is the parent directory 
+containing the subdirectories to be bagged
+
+
+DETAILED INSTRUCTIONS
+
+1. Open the [terminal](http://www.westwind.com/reference/os-x/commandline/navigation.html) or [command prompt](http://ss64.com/nt/cd.html) and navigate to the directory containing bagbatch.py. For example, if it's in a folder on the desktop, enter "cd Desktop/folder".
+2. Enter (without the quotes) "python bagbatch.py <dir>". <dir> is the 
+directory that contains all the subdirectories that need to be bagged. Type 
+the path in or drag-and-drop the folder from the Finder/Explorer menu. Press 
+enter and the current status will be printed to the screen. If prompted for 
+the directory containing bag.sh (Mac) or bag.bat (Windows), enter the 
+installation directory. This end of this folder name will look like 
+"\bagit-VERSION\bin". Depending on the size of the files, it may take a while 
+to bag. The program is done once "Bags complete" is printed.
+
+
+
+Changing the BagIt installation path used by bagbatch.py 
+--------------------------------------------------------
+OVERVIEW
+
+ - Option 1: Delete BAGIT_INST_PATH.txt and run bagbatch.py. Enter the 
+ location of the new installation.
+ - Option 2: Manually edit BAGIT_INST_PATH.txt to the location of the new 
+ installation
+
+
+DETAILED INSTRUCTIONS
+
+BAGIT_INST_PATH.txt contains the path to BagIt's bag installation. This path 
+will look similar to "C:/Program Files/bagit-VERSION/bin" or "/Program Files/
+Archival/bagit-4.9.0/bin". Do NOT use ".../src/assembly/bin", use 
+".../bagit-x.x.x-bin/bagit-x.x.x/bin" instead. Each time bagbatch.py runs, it 
+will validate this path, checking to see that the directory and bag.sh (Mac) 
+or bag.bat (Windows) exists. If the directory does not exists or it does not 
+contain bag.sh or bag.bat, it will prompt for the correct path. If a new 
+version of BagIt is installed and the old version is not removed, 
+BAGIT_INST_PATH.txt must be manually edited. Either delete the file and run 
+bagbatch.py and let it prompt for the installation directory, entering the new 
+installation this time, or enter in the path to the new installation.
